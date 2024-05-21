672fd6c13870596eeac48de0d74ccb828e7359e8
Khaled Nasser <2khaled.alsaeedi@gmail.com>
2024-05-21 18:13:20 +0300

    add style to JSX elements

--- pizza/src/MyfirstCompoment.js
+++ pizza/src/MyfirstCompoment.js
@@ -2,7 +2,7 @@ export default function MyfirstCompoment() {
   return (
     <div>
       <h1>My First Compoment in React.js</h1>
-      <button id="btn">Click Me</button>
+      <button>Click Me</button>
     </div>
   );
 }
--- pizza/src/SecondComponets.js
+++ pizza/src/SecondComponets.js
@@ -1,8 +1,24 @@
-export default function SecondCompoents(){
-    return (
-      <div>
-        <h1>HI Welcome !!</h1>
-        <a src='#btn'>Click</a>
-      </div>
-    );
-  }
+export default function SecondCompoents() {
+  const eleStyle = {
+    backgroundColor: "blue",
+    fontSize: "16px",
+    fontWeight: "800",
+    padding: "5px 16px",
+  };
+  return (
+    <div>
+      <h1
+        style={{
+          fontSize: "48px",
+          color: "red",
+        }}
+      >
+        HI Welcome !!
+      </h1>
+      <a style={eleStyle}>Click</a>
+    </div>
+  );
+}
+// Notes : there're more then one way to add style in JSX elements :
+// 1- create object and add the style of elements and add the style to elements by style={name of objects} like line  2-7
+// 2- add the style dierct to the elements by style={{style}} like line 10-12
