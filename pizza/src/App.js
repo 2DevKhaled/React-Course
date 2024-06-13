aeb601cf3bb64ba1ad4d42e021b2e849602989b2
Khaled Nasser <2khaled.alsaeedi@gmail.com>
2024-06-13 16:25:56 +0300

    Handling the State and Updating

--- pizza/src/App.js
+++ pizza/src/App.js
@@ -1,13 +1,54 @@
 import "./App.css";
-import Posts from "./Posts"
-function App() {
+import { useState } from "react";
+const messages = ["Learn React ", "Apply for jobs", "Invest your new income"];
 
+function App() {
+  const [isOpen,setIsOpen] = useState(true);
+  const [step, setStep] = useState(1);
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+  function handleNext() {
+    if (step < 3) {
+      setStep(step + 1);
+    }else if(step==3){
+      setIsOpen(false);
+    }
+  }
+  function handelOpen(){
+    setIsOpen(!isOpen)
+  }
   return (
-    <div className="app">
-      <div>
-      <Posts/>
+    <>
+      <button className="close" onClick={handelOpen}><i class="ri-close-line"></i></button>
+      {
+      isOpen !== true?<div></div>:
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
-    </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+        
+      </p>
+      <div className="buttons">
+        <button
+          onClick={handlePrevious}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
+          Previous
+        </button>
+        <button
+          onClick={handleNext}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
+          Next
+        </button>
+      </div>
+    </div>}
+    </>
   );
 }
 export default App;
