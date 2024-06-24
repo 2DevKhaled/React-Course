cb7361ed925ef84dba0ca2962019e115ec53cda1
Khaled Nasser <2khaled.alsaeedi@gmail.com>
2024-06-25 02:05:09 +0300

    Project travel list Not Compelted

--- pizza/src/App.js
+++ pizza/src/App.js
@@ -1,44 +1,75 @@
-import "./App.css";
 import { useState } from "react";
-const messages = ["Learn React ", "Apply for jobs", "Invest your new income"];
-
-function App() {
-  const [step, setStep] = useState(1);
-  const [count, setCount] = useState(0);
-  const date = new Date("june 21 2024");
-  date.setDate(date.getDate() +count)
+const intialItems = [
+  { id: 1, description: "Passports", quanttiy: 2, packed: false },
+  { id: 2, description: "Socks", quanttiy: 12, packed: true },
+];
+export default function App() {
   return (
-    <div className="dateCounter">
-      <div className="steps">
-        <div ckassName="buttons">
-          <button onClick={() => setStep((s) => s - 1)}>
-            <span>-</span>
-          </button>
-          <p>Step:{step}</p>
-          <button onClick={() => setStep((s) => s + 1)}>
-            <span>+</span>
-          </button>
-        </div>
-      </div>
-      <hr />
-      <div className="counter">
-        <div ckassName="buttons">
-          <button onClick={() => setCount((c) => c - step)}>
-            <span>-</span>
-          </button>
-          <p>Count:{count}</p>
-          <button onClick={() => setCount((c) => c + step)}>
-            <span>+</span>
-          </button>
-        </div>
-      </div>
-      <div className="date">
-        <p>
-          {count === 0 ? "Today is " : count>0 ?` ${count} days from today is: `: `${Math.abs(count)} days ago was`}
-          {date.toDateString()}
-        </p>
-      </div>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
-export default App;
+function Logo() {
+  return <h1>🌱 Far Away 👜</h1>;
+}
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quanttiy, setQuanttiy] = useState(1);
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quanttiy, package: false, id: Date.now() };
+    setDescription("");
+    setQuanttiy(1);
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip</h3>
+      <select
+        value={quanttiy}
+        onChange={(e) => setQuanttiy(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {intialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quanttiy} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+function Stats() {
+  <footer>You Have X items on your list,and you already packed X (x%)</footer>;
+}
